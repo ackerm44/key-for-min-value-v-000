05e0fa3567d0f8ff22faae735f8428a7145544bd
Amanda Ackerman <ackerm44@gmail.com>
2017-10-24 23:13:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,11 @@ def key_for_min_value(name_hash)
   name_hash.collect do |item, num|
     collection << num
   end
-  collection
+  if collection[0] < collection[1] && collection[0] < collection [2]
+    return collection[0]
+  elsif collection[1] < collection[0] && collection[1] < collection [2]
+    return collection[1]
+  elsif collection[2] < collection[0] && collection[0] < collection [1]
+    return collection[2]
+  end
 end
